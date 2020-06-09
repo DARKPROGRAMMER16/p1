53c35cfcb8bf39b6e7b2a5b8651a1d066d0a3b45
Rajat Dash <rohitdash16.rdx@gmail.com>
2020-06-09 12:50:19 +0530

    templates added

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render,HttpResponse
 
 # Create your views here.
 def bloghome(request):
-    return HttpResponse('this is blog home')
+    # return HttpResponse('this is blog home')
+    return render(request,'blog/bloghome.html')
 def blogpost(request,slug):
-    return HttpResponse(f'this is blog post: {slug}')
+    # return HttpResponse(f'this is blog post: {slug}')
+    return render(request,'blog/blogpost.html')
